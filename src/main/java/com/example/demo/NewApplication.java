141b8271ce31a0d064aa4487d3848a21cbde952b
balaram365 <balaramakrishnaundavalli@gmail.com>
2026-07-23 16:05:12 +0530

    connected to database

--- src/main/java/com/example/demo/NewApplication.java
+++ src/main/java/com/example/demo/NewApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import com.example.demo.controller.TestRestEndPoint;
+import com.example.demo.dao.StudentDAO;
+import com.example.demo.entity.Student;
 
 @SpringBootApplication
 public class NewApplication {
@@ -24,7 +28,22 @@ public class NewApplication {
 		
 		//ioc -> spring inverson controller of beans
 //		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	}
+		@Bean
+		public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+			return runner->{
+				createStudent(studentDAO);
+			};
+			
+		}
+		private void createStudent(StudentDAO studentDAO) {
+			// TODO Auto-generated method stub
+			Student details = new Student("Balaram","krishna","balaram@gamil.com");
+			studentDAO.save(details);
+			
+			System.out.println("save id "+details.getId());
+		}
 		
 	}
 
-}
+
